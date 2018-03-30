691a063448ecb905d98d4d5eac6efd3a5c1a85f0
Roberto C Navarro <rnavarro70@gmail.com>
2018-03-29 20:24:09 -0500

    two video objects

--- EventsAndDelegates/Program.cs
+++ EventsAndDelegates/Program.cs
@@ -7,7 +7,12 @@ namespace EventsAndDelegates
     {
         static void Main()
         {
-            var video = new Video() { Title = "Video 1" };
+            var video = new Video { Title = "Video 1" };
+
+            var video2 = new Video { Title = "Video 2" };
+
+
+
             var videoEncoder = new VideoEncoder(); // publisher
             var mailService = new MailService(); // subscriber
             var messageService = new MessageService(); // subscriber
@@ -16,25 +21,31 @@ namespace EventsAndDelegates
             videoEncoder.VideoEncoded += messageService.OnVideoEncoded;
 
             videoEncoder.Encode(video);
+            videoEncoder.Encode(video2);
 
             Console.WriteLine("Press [Enter] to close application.");
             Console.ReadLine();
         }
     }
 
+    class VideoEventArgs : EventArgs
+    {
+        public Video Clip { get; set; }
+    }
+
     class MailService
     {
-        public void OnVideoEncoded(object source, EventArgs e)
+        public void OnVideoEncoded(object source, VideoEventArgs args)
         {
-            Console.WriteLine("MailService: Sending an email...");
+            Console.WriteLine(args.Clip.Title + " - MailService: Sending an email...");
         }
     }
 
     class MessageService
     {
-        public void OnVideoEncoded(object source, EventArgs e)
+        public void OnVideoEncoded(object source, VideoEventArgs args)
         {
-            Console.WriteLine("MessageService: Sending a text message...");
+            Console.WriteLine(args.Clip.Title + " - MessageService: Sending a text message...");
         }
     }
 
@@ -44,7 +55,7 @@ namespace EventsAndDelegates
         // 2. Define an event based on the delegate
         // 3. Raise the event
 
-        public delegate void VideoEncodedEventHandler(object source, EventArgs args);
+        public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);
 
         public event VideoEncodedEventHandler VideoEncoded;
 
@@ -53,17 +64,18 @@ namespace EventsAndDelegates
             Console.WriteLine("Encoding Video...");
             Thread.Sleep(3000);
 
-            OnVideoEncoded();
+            OnVideoEncoded(video);
         }
 
-        protected virtual void OnVideoEncoded()
+        protected virtual void OnVideoEncoded(Video video)
         {
             //if (VideoEncoded != null)
             //{
             //    VideoEncoded(this, EventArgs.Empty);
             //}
+            var vEvents = new VideoEventArgs { Clip = video };
 
-            VideoEncoded?.Invoke(this, EventArgs.Empty); // see: http://bit.ly/2ujCs1F
+            VideoEncoded?.Invoke(this, vEvents); // see: http://bit.ly/2ujCs1F
         }
     }
 
